9402f3ff774ffea7648420645a51ad8a017ef7cc
Gabriel Portelinha Rico <155638603+gportrico@users.noreply.github.com>
2025-06-22 18:52:16 +0000

    Customização do Botão Login e Cadastro

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ export const Button = ({ children, onClick, type = "button", ...props }) => (
   <button
     type={type}
     onClick={onClick}
-    className="button"
+    className="button bg-gradient-to-b from-orange-400 to-orange-600 text-white font-semibold py-2 px-4 rounded shadow"
     {...props}
   >
     {children}
