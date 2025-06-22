347803c37a7224b308990e19d165788238588624
Gabriel Portelinha Rico <155638603+gportrico@users.noreply.github.com>
2025-06-22 18:15:21 +0000

    Customização de Logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,9 +1,15 @@
 import "./logo.css";
+import logo from "../../assets/images/logo.png";
 
 export const Logo = () => {
   return (
     <div className="logo">
-      <div className="text-wrapper">Logotipo</div>
+      <img src={logo} alt="Logo" className="logo-img" />
+      <div
+        className="text-7xl font-bold bg-gradient-to-r from-orange-400 via-orange-500 to-orange-600 bg-clip-text text-transparent"
+      >
+        Happenize
+      </div>
     </div>
   );
 };
